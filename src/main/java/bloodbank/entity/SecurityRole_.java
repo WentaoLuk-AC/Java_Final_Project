02e19e2c99452a85ae7407ba149c48e98c610a35
JC <jacob.d.crocker@gmail.com>
2022-08-13 15:01:28 -0400

    initial commit

---
+++ src/main/java/bloodbank/entity/SecurityRole_.java
@@ -0,0 +1,14 @@
+package bloodbank.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2022-08-13T14:28:29.127-0400")
+@StaticMetamodel(SecurityRole.class)
+public class SecurityRole_ {
+	public static volatile SingularAttribute<SecurityRole, Integer> id;
+	public static volatile SingularAttribute<SecurityRole, String> roleName;
+	public static volatile SetAttribute<SecurityRole, SecurityUser> users;
+}
